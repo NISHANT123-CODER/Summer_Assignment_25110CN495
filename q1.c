7c2f615e16ac47ea3bdd1973ecaf2c384765898c
NISHANT123-CODER <yanishant2007@gmail.com>
2026-05-28 17:08:57 +0530

    Add new codes

---
+++ q1.c
@@ -0,0 +1,11 @@
+//write a program to calculate sum of first N natural number
+#include<stdio.h>
+int main(){
+    int n, sum=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    sum=sum+i;
+    printf("sum=%d",sum);
+    return 0;
+}
